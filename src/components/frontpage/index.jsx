47b71136af69f578d752096232a335dc30ec3b30
Thomas Henriksen <thomashenriksen832@hotmail.com>
2023-07-08 21:11:08 +0200

    updated images

--- src/components/frontpage/index.jsx
+++ src/components/frontpage/index.jsx
@@ -1,11 +1,10 @@
 import * as Ho from "./index.styled"
-import Thomas from "/home/snurre/myportfolio/myporfolio/src/Thomas.jpg"
 function Home() {
     return (
       <Ho.Wrapper>
         <h1>Portfolio</h1>
         <p>Hello and welcome!</p>
-        <Ho.ImageContainer><Ho.Image src={Thomas} alt="of Thomas Henriksen"></Ho.Image></Ho.ImageContainer>
+        <Ho.ImageContainer><Ho.Image src={require('/home/snurre/myportfolio/myporfolio/src/Thomas.jpg')} alt="of Thomas Henriksen"></Ho.Image></Ho.ImageContainer>
         <p>Yes, that`s me inside a tunnel</p>
        </Ho.Wrapper>
     );
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,10 +1,5 @@
 import * as M from "./index.styled"
-import CSM from "/home/snurre/myportfolio/myporfolio/src/csmscr.png"
-import Gamehub from "/home/snurre/myportfolio/myporfolio/src/gamehubscr.png"
-import FoodCorner from "/home/snurre/myportfolio/myporfolio/src/foodcornerscr.png"
-import Lux from "/home/snurre/myportfolio/myporfolio/src/luuuux.png"
-import YourBnb from "/home/snurre/myportfolio/myporfolio/src/Your.png"
-import YourStore from "/home/snurre/myportfolio/myporfolio/src/yourstore.png"
+
 function Projects() {
     return (
       <>
@@ -12,7 +7,7 @@ function Projects() {
         <M.Container>
             <M.CardContainer>
                 <h2>Gamehub</h2>
-                <M.ImageContainer><M.Image src={Gamehub} alt="Gamehub site"></M.Image></M.ImageContainer>
+                <M.ImageContainer><M.Image src={require('/home/snurre/myportfolio/myporfolio/src/gamehubscr.png')} alt="Gamehub site"></M.Image></M.ImageContainer>
                 <M.TextContainer>My first proper attemt at a site, this was my first project at Noroff. This was only made with HTML and CSS, later I attemted some JS from our first session with it.
                 </M.TextContainer>
                 <M.UlDecor>
@@ -22,7 +17,7 @@ function Projects() {
             </M.CardContainer>
             <M.CardContainer>
                 <h2>Community Science Museum</h2>
-                <M.ImageContainer><M.Image src={CSM} alt="Community Science Museum site"></M.Image></M.ImageContainer>
+                <M.ImageContainer><M.Image src={require('/home/snurre/myportfolio/myporfolio/src/csmscr.png')} alt="Community Science Museum site"></M.Image></M.ImageContainer>
                 <M.TextContainer>My semester project at Noroff, made with HTML and CSS. First time trying media queries</M.TextContainer>
                 <M.UlDecor>
                 <li><a href="https://sleepy-einstein-abf439.netlify.app">Link to Site</a></li>
@@ -31,7 +26,7 @@ function Projects() {
             </M.CardContainer>
             <M.CardContainer>
                 <h2>Foodcorner</h2>
-                <M.ImageContainer><M.Image src={FoodCorner} alt="Foodcorner site"></M.Image></M.ImageContainer>
+                <M.ImageContainer><M.Image src={require('/home/snurre/myportfolio/myporfolio/src/foodcornerscr.png')} alt="Foodcorner site"></M.Image></M.ImageContainer>
                 <M.TextContainer>Project exam for my first year at Noroff, made with HTML, CSS and JS.</M.TextContainer>
                 <M.UlDecor>
                 <li><a href="https://github.com/Noroff-FEU-Assignments/project-exam-1-Snurre1">Link to Repo</a></li>
@@ -39,7 +34,7 @@ function Projects() {
             </M.CardContainer>
             <M.CardContainer>
                 <h2>Lux</h2>
-                <M.ImageContainer><M.Image src={Lux} alt="Kux site"></M.Image></M.ImageContainer>
+                <M.ImageContainer><M.Image src={require('/home/snurre/myportfolio/myporfolio/src/luuuux.png')} alt="Kux site"></M.Image></M.ImageContainer>
                 <M.TextContainer>This is my semester exam for my second year at Noroff. Made with HTML, CSS, JS, Bootstrap and Sass</M.TextContainer>
                 <M.UlDecor>
                 <li><a href="https://tubular-selkie-6ac3da.netlify.app">Link to Site</a></li>
@@ -48,7 +43,7 @@ function Projects() {
             </M.CardContainer>
             <M.CardContainer>
                 <h2>YourStore</h2>
-                <M.ImageContainer><M.Image src={YourStore} alt="YourStore site"></M.Image></M.ImageContainer>
+                <M.ImageContainer><M.Image src={require('/home/snurre/myportfolio/myporfolio/src/yourstore.png')} alt="YourStore site"></M.Image></M.ImageContainer>
                 <M.TextContainer>My first React app! Created with HTML, JS, React and Styled Components.</M.TextContainer>
                 <M.UlDecor>
                 <li><a href="https://transcendent-unicorn-004ef6.netlify.app">Link to Site</a></li>
@@ -57,7 +52,7 @@ function Projects() {
             </M.CardContainer>
             <M.CardContainer>
                 <h2>YourBNB</h2>
-                <M.ImageContainer><M.Image src={YourBnb} alt="YourBNB site"></M.Image></M.ImageContainer>
+                <M.ImageContainer><M.Image src={require('/home/snurre/myportfolio/myporfolio/src/Your.png')} alt="YourBNB site"></M.Image></M.ImageContainer>
                 <M.TextContainer>This was my final exam at Noroff, a booking site. This was made with HTML, JS, React and Styled Components.</M.TextContainer>
                 <M.UlDecor>
                 <li><a href="https://jade-florentine-9e175f.netlify.app/">Link to Site</a></li>
