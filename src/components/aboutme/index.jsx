6f58fbb7a3d3c964d5f8fc87399dd4a62b2b78cc
Thomas Henriksen <thomashenriksen832@hotmail.com>
2023-07-08 20:37:43 +0200

    Update index.jsx

--- src/components/aboutme/index.jsx
+++ src/components/aboutme/index.jsx
@@ -9,6 +9,8 @@ function AboutMe() {
        <A.Container>I`m a frontend developer, and finished my studies at Noroff in a 2 year course.</A.Container>
        <A.Container>From that course I learned about HTML, CSS and JS. I also learned about frameworks such as Bootstrap, Sass and React.</A.Container>
        <A.Container>Other than that I learned about design, different methodologies like scrum and kanban, and experience of setting up a real app in teams.</A.Container>
+       <A.Container>You can reach me through my LinkedIn page: <a href="https://www.linkedin.com/in/thomas-henriksen-60334321a/">Link</a></A.Container>
+       
       </div>
     );
   }
